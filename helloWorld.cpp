160f68da2f20aecac9df79ef5fd9309c3bf5999d
Akash <cleverakash1@gmail.com>
2021-08-02 08:26:57 +0530

    Starting with C++

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+// The header line is importing and header file named iostream which is used to perfom functionallity related to input/output
+
+
+// int-> integer( int is used so that we are defining that the main ouput/return will be an integer) and main() is an function which is an entry point and from which the real execution starts
+
+int main(){
+    std::cout <<"Hello World "<<std::endl;
+    // cout is an method of std namespace which is inside iostream
+    return 0;
+}
---
+++ variable_DT.cpp
@@ -0,0 +1,15 @@
+// Variable and Datatypes
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // int num1 = 4;
+    // int num2 = 7;
+    int num1 = 4, num2 = 7;
+    float deci = 3.412;
+    char name = 'A';
+    cout<<"The sum of "<<num1<<" and "<<num2<<" is : "<<num1+num2<<endl;
+    cout<<"My Name starts with : "<<name<<" and "<<deci<<"\n";
+    return 0;
+}
