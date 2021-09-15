c642a0ffc1f909b3dd35e59aaa52669b00a897d4
Akash <dipu64601@gmail.com>
2021-09-15 23:25:58 +0530

    Practicing more Pattern Question

--- practice/patternQues.cpp
+++ practice/patternQues.cpp
@@ -261,5 +261,107 @@ int main(){
         cout<<"*\n";
     }
 
+
+    /*  16.         1 
+              2 1 2 
+            3 2 1 2 3 
+          4 3 2 1 2 3 4 
+        5 4 3 2 1 2 3 4 5        */
+    for (int i=1; i<=5; i++){
+        for (int j=0; j<5-i; j++){
+            cout<<"  ";
+        }
+        for (int k=i; k>=1; k--){
+            cout<<k<<" ";
+        }
+        for (int j=2; j<=i; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+
+/*  17.     1      1
+           212
+          32123
+         4321234
+        543212345
+         4321234
+          32123
+           212
+            1          */
+    for (int i=1; i<=9; i++){
+        int space = (i <= 5)? 5-i : i-5;
+        for (int j=0; j<space; j++){
+            cout<<" ";
+        }
+        for (int k=5-space; k>=1; k--){
+            cout<<k;
+        }
+        for (int j=2; j<=5-space; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+
+    /* 18        *
+                * *
+               * * *
+              * * * *
+             * * * * *
+              * * * *
+               * * *
+                * *
+                 *              */
+    for (int i=0; i<9; i++){
+        int space = (i < 5)? 5-i-1 : i-5+1;
+        for (int j=0; j<space; j++){
+            cout<<" ";
+        }
+        int star = 5-space;
+        for (int k=0; k<star; k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+
+/*  19.     0 0 0 0 0 0 0 0 0
+            0 1 1 1 1 1 1 1 0 
+            0 1 2 2 2 2 2 1 0
+            0 1 2 3 3 3 2 1 0
+            0 1 2 3 4 3 2 1 0
+            0 1 2 3 3 3 2 1 0
+            0 1 2 2 2 2 2 1 0
+            0 1 1 1 1 1 1 1 0
+            0 0 0 0 0 0 0 0 0          */
+    int n = 4;
+    for (int row=0; row<=2*n; row++){
+        for (int col=0; col<=2*n; col++){
+            int minNum = min(min(row, col), min(2*n-col, 2*n-row));
+            cout<<minNum<<" ";
+        }
+        cout<<endl;
+    }
+
+
+/*  20.     4 4 4 4 4 4 4  
+            4 3 3 3 3 3 4   
+            4 3 2 2 2 3 4   
+            4 3 2 1 2 3 4   
+            4 3 2 2 2 3 4   
+            4 3 3 3 3 3 4   
+            4 4 4 4 4 4 4          */
+    n = 4;
+    for (int row=0; row<2*n-1; row++){
+        for (int col=0; col<2*n-1; col++){
+            int minNum = min(min(row, col), min(2*n-col-2, 2*n-row-2));
+            cout<<n-minNum<<" ";
+        }
+        cout<<endl;
+    }
+
+
     return 0;
 }
